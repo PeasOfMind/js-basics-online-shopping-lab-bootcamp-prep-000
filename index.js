2c0dca36b9a562fe5050163555a7eb0fc6c087d6
Yeqing Liu <sunsetpeak42@gmail.com>
2018-07-14 04:14:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function setCart(c) {
 function addToCart(item) {
   let newItem = {itemName: item, itemPrice: Math.floor(Math.random()*10 + 1)};
   cart.push(newItem)
-  return `${item} has been added to your cart`;
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  
 }
 
 function total() {
